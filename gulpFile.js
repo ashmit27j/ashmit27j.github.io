a0cbc4174e2a6f1bb96a857a67ebdac6d8b544c6
Ashmit <ashmit27j@gmail.com>
2024-09-01 14:04:00 +0530

    add: gulpFile, fix: marquee, note: fix fonts next

---
+++ gulpFile.js
@@ -0,0 +1,24 @@
+const gulp = require("gulp");
+const postcss = require("gulp-postcss");
+const tailwindcss = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+
+gulp.task("css", function () {
+	return gulp
+		.src("src/css/input.css") // Path to your input CSS file
+		.pipe(
+			postcss([
+				tailwindcss(),
+				autoprefixer(),
+				cssnano(), // Minify the output CSS
+			])
+		)
+		.pipe(gulp.dest("css/output.css")); // Destination folder for the output CSS
+});
+
+gulp.task("watch", function () {
+	gulp.watch("src/input.css", gulp.series("css"));
+});
+
+gulp.task("default", gulp.series("css", "watch"));
