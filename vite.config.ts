1702a33b1680d6101e435bc1d686ec3fe6883a44
ashmit27j <ashmit27j@gmail.com>
2026-03-03 02:02:34 +0530

    added base to vite config - dnpb

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
+
 export default defineConfig(({ mode }) => ({
+  base: "/", 
   server: {
     host: "::",
     port: 8080,
